3e82c4480287a4863afc6f04890f353e7e1bae61
kirrie <kirrie@gmail.com>
2017-07-25 22:46:02 +0900

    - rewrite js-shortcuts into es6 (keycuts)

--- dist/keycuts.js
+++ dist/keycuts.js
@@ -1,213 +1,281 @@
 'use strict';
 
-/**
- * http://www.openjs.com/scripts/events/keyboard_shortcuts/
- * Version : 2.01.B
- * By Binny V A
- * License : BSD
- */
-shortcut = {
-	'all_shortcuts': {}, //All the shortcuts are stored in this array
-	'add': function add(shortcut_combination, callback, opt) {
-		//Provide a set of default options
-		var default_options = {
-			'type': 'keydown',
-			'propagate': false,
-			'disable_in_input': false,
-			'target': document,
-			'keycode': false
-		};
-		if (!opt) opt = default_options;else {
-			for (var dfo in default_options) {
-				if (typeof opt[dfo] == 'undefined') opt[dfo] = default_options[dfo];
-			}
-		}
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-		var ele = opt.target;
-		if (typeof opt.target == 'string') ele = document.getElementById(opt.target);
-		var ths = this;
-		shortcut_combination = shortcut_combination.toLowerCase();
+var _underscore = require('underscore');
 
-		//The function to be called at keypress
-		var func = function func(e) {
-			e = e || window.event;
+var _underscore2 = _interopRequireDefault(_underscore);
 
-			if (opt['disable_in_input']) {
-				//Don't enable shortcut keys in Input, Textarea fields
-				var element;
-				if (e.target) element = e.target;else if (e.srcElement) element = e.srcElement;
-				if (element.nodeType == 3) element = element.parentNode;
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-				if (element.tagName == 'INPUT' || element.tagName == 'TEXTAREA') return;
-			}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-			//Find Which key is pressed
-			if (e.keyCode) code = e.keyCode;else if (e.which) code = e.which;
-			var character = String.fromCharCode(code).toLowerCase();
+var SHIFTNUMS = {
+	'`': '~',
+	'1': '!',
+	'2': '@',
+	'3': '#',
+	'4': '$',
+	'5': '%',
+	'6': '^',
+	'7': '&',
+	'8': '*',
+	'9': '(',
+	'0': ')',
+	'-': '_',
+	'=': '+',
+	';': ':',
+	"'": '"',
+	',': '<',
+	'.': '>',
+	'/': '?',
+	'\\': '|'
+};
 
-			if (code == 188) character = ","; //If the user presses , when the type is onkeydown
-			if (code == 190) character = "."; //If the user presses , when the type is onkeydown
+var SPECIALKEYS = {
+	esc: 27,
+	escape: 27,
+	tab: 9,
+	space: 32,
+	return: 13,
+	enter: 13,
+	backspace: 8,
+	scrolllock: 145,
+	scroll_lock: 145,
+	scroll: 145,
+	capslock: 20,
+	caps_lock: 20,
+	caps: 20,
+	numlock: 144,
+	num_lock: 144,
+	num: 144,
+	pause: 19,
+	break: 19,
+	insert: 45,
+	home: 36,
+	delete: 46,
+	end: 35,
+	pageup: 33,
+	page_up: 33,
+	pu: 33,
+	pagedown: 34,
+	page_down: 34,
+	pd: 34,
+	left: 37,
+	up: 38,
+	right: 39,
+	down: 40,
+	f1: 112,
+	f2: 113,
+	f3: 114,
+	f4: 115,
+	f5: 116,
+	f6: 117,
+	f7: 118,
+	f8: 119,
+	f9: 120,
+	f10: 121,
+	f11: 122,
+	f12: 123
+};
 
-			var keys = shortcut_combination.split("+");
-			//Key Pressed - counts the number of valid keypresses - if it is same as the number of keys, the shortcut function is invoked
-			var kp = 0;
-
-			//Work around for stupid Shift key bug created by using lowercase - as a result the shift+num combination was broken
-			var shift_nums = {
-				"`": "~",
-				"1": "!",
-				"2": "@",
-				"3": "#",
-				"4": "$",
-				"5": "%",
-				"6": "^",
-				"7": "&",
-				"8": "*",
-				"9": "(",
-				"0": ")",
-				"-": "_",
-				"=": "+",
-				";": ":",
-				"'": "\"",
-				",": "<",
-				".": ">",
-				"/": "?",
-				"\\": "|"
-				//Special Keys - and their codes
-			};var special_keys = {
-				'esc': 27,
-				'escape': 27,
-				'tab': 9,
-				'space': 32,
-				'return': 13,
-				'enter': 13,
-				'backspace': 8,
-
-				'scrolllock': 145,
-				'scroll_lock': 145,
-				'scroll': 145,
-				'capslock': 20,
-				'caps_lock': 20,
-				'caps': 20,
-				'numlock': 144,
-				'num_lock': 144,
-				'num': 144,
-
-				'pause': 19,
-				'break': 19,
-
-				'insert': 45,
-				'home': 36,
-				'delete': 46,
-				'end': 35,
-
-				'pageup': 33,
-				'page_up': 33,
-				'pu': 33,
-
-				'pagedown': 34,
-				'page_down': 34,
-				'pd': 34,
-
-				'left': 37,
-				'up': 38,
-				'right': 39,
-				'down': 40,
-
-				'f1': 112,
-				'f2': 113,
-				'f3': 114,
-				'f4': 115,
-				'f5': 116,
-				'f6': 117,
-				'f7': 118,
-				'f8': 119,
-				'f9': 120,
-				'f10': 121,
-				'f11': 122,
-				'f12': 123
-			};
-
-			var modifiers = {
-				shift: { wanted: false, pressed: false },
-				ctrl: { wanted: false, pressed: false },
-				alt: { wanted: false, pressed: false },
-				meta: { wanted: false, pressed: false //Meta is Mac specific
-				} };
-
-			if (e.ctrlKey) modifiers.ctrl.pressed = true;
-			if (e.shiftKey) modifiers.shift.pressed = true;
-			if (e.altKey) modifiers.alt.pressed = true;
-			if (e.metaKey) modifiers.meta.pressed = true;
-
-			for (var i = 0; k = keys[i], i < keys.length; i++) {
-				//Modifiers
-				if (k == 'ctrl' || k == 'control') {
-					kp++;
-					modifiers.ctrl.wanted = true;
-				} else if (k == 'shift') {
-					kp++;
-					modifiers.shift.wanted = true;
-				} else if (k == 'alt') {
-					kp++;
-					modifiers.alt.wanted = true;
-				} else if (k == 'meta') {
-					kp++;
-					modifiers.meta.wanted = true;
-				} else if (k.length > 1) {
-					//If it is a special key
-					if (special_keys[k] == code) kp++;
-				} else if (opt['keycode']) {
-					if (opt['keycode'] == code) kp++;
-				} else {
-					//The special keys did not match
-					if (character == k) kp++;else {
-						if (shift_nums[character] && e.shiftKey) {
-							//Stupid Shift key bug created by using lowercase
-							character = shift_nums[character];
-							if (character == k) kp++;
-						}
-					}
-				}
-			}
-
-			if (kp == keys.length && modifiers.ctrl.pressed == modifiers.ctrl.wanted && modifiers.shift.pressed == modifiers.shift.wanted && modifiers.alt.pressed == modifiers.alt.wanted && modifiers.meta.pressed == modifiers.meta.wanted) {
-				callback(e);
-
-				if (!opt['propagate']) {
-					//Stop the event
-					//e.cancelBubble is supported by IE - this will kill the bubbling process.
-					e.cancelBubble = true;
-					e.returnValue = false;
-
-					//e.stopPropagation works in Firefox.
-					if (e.stopPropagation) {
-						e.stopPropagation();
-						e.preventDefault();
-					}
-					return false;
-				}
-			}
-		};
-		this.all_shortcuts[shortcut_combination] = {
-			'callback': func,
-			'target': ele,
-			'event': opt['type']
-		};
-		//Attach the function with the event
-		if (ele.addEventListener) ele.addEventListener(opt['type'], func, false);else if (ele.attachEvent) ele.attachEvent('on' + opt['type'], func);else ele['on' + opt['type']] = func;
+var MODIFIERS = {
+	shift: {
+		wanted: false,
+		pressed: false
 	},
-
-	//Remove the shortcut - just specify the shortcut and I will remove the binding
-	'remove': function remove(shortcut_combination) {
-		shortcut_combination = shortcut_combination.toLowerCase();
-		var binding = this.all_shortcuts[shortcut_combination];
-		delete this.all_shortcuts[shortcut_combination];
-		if (!binding) return;
-		var type = binding['event'];
-		var ele = binding['target'];
-		var callback = binding['callback'];
-
-		if (ele.detachEvent) ele.detachEvent('on' + type, callback);else if (ele.removeEventListener) ele.removeEventListener(type, callback, false);else ele['on' + type] = false;
+	ctrl: {
+		wanted: false,
+		pressed: false
+	},
+	alt: {
+		wanted: false,
+		pressed: false
+	},
+	meta: {
+		wanted: false,
+		pressed: false
 	}
 };
+
+var Keycuts = function () {
+	function Keycuts() {
+		_classCallCheck(this, Keycuts);
+
+		this.defaultShortcutOption = {
+			type: 'keydown',
+			propagate: false,
+			disableInInput: false,
+			target: document,
+			keycode: false
+		};
+		this.allShortcuts = {};
+	}
+
+	_createClass(Keycuts, [{
+		key: 'add',
+		value: function add(shortcutCombination, callback) {
+			var option = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
+
+			option = _underscore2.default.defaults(option, this.defaultShortcutOption);
+			shortcutCombination = shortcutCombination.toLowerCase();
+			var targetElement = _underscore2.default.isString(option.target) ? document.getElementById(option.target) : option.target;
+			var handler = function handler(e) {
+				var char = void 0,
+				    code = void 0,
+				    kp = 0,
+				    modifiers = {
+					shift: {
+						wanted: false,
+						pressed: false
+					},
+					ctrl: {
+						wanted: false,
+						pressed: false
+					},
+					alt: {
+						wanted: false,
+						pressed: false
+					},
+					meta: {
+						wanted: false,
+						pressed: false
+					}
+				};
+
+				e = e || window.event;
+
+				if (option.disableInInput) {
+					var element = e.target ? e.target : e.srcElement ? e.srcElement : null;
+
+					if (element !== null && element.nodeType === 3) {
+						element = element.parentNode;
+					}
+
+					if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
+						return false;
+					}
+				}
+
+				if (e.keyCode) {
+					code = e.keyCode;
+				} else if (e.which) {
+					code = e.which;
+				}
+
+				char = code === 188 ? ',' : code === 190 ? '.' : String.fromCharCode(code).toLowerCase();
+
+				if (e.ctrlKey) {
+					modifiers.ctrl.pressed = true;
+				}
+				if (e.shiftKey) {
+					modifiers.shift.pressed = true;
+				}
+				if (e.altKey) {
+					modifiers.alt.pressed = true;
+				}
+				if (e.metaKey) {
+					modifiers.meta.pressed = true;
+				}
+
+				shortcutCombination.split('+').forEach(function (key) {
+					switch (key) {
+						case 'ctrl':
+						case 'control':
+							kp++;
+							modifiers.ctrl.wanted = true;
+							break;
+						case 'shift':
+							kp++;
+							modifiers.shift.wanted = true;
+							break;
+						case 'alt':
+							kp++;
+							modifiers.alt.wanted = true;
+							break;
+						case 'meta':
+							kp++;
+							modifiers.meta.wanted = true;
+							break;
+						default:
+							if (key.length > 1 && SPECIALKEYS[key] === code) {
+								kp++;
+							} else if (option.keycode) {
+								kp++;
+							} else {
+								if (char === key) {
+									kp++;
+								} else {
+									if (SHIFTNUMS[char] && e.shiftKey) {
+										char = SHIFTNUMS[char];
+										if (char === k) {
+											kp++;
+										}
+									}
+								}
+							}
+							break;
+					}
+				});
+
+				if (kp == keys.length && modifiers.ctrl.pressed === modifiers.ctrl.wanted && modifiers.shift.pressed === modifiers.shift.wanted && modifiers.alt.pressed === modifiers.alt.wanted && modifiers.meta.pressed === modifiers.meta.wanted) {
+					callback(e);
+
+					if (!option.propagate) {
+						e.cancelBubble = true;
+						e.returnValue = false;
+
+						if (e.stopPropagation) {
+							e.stopPropagation();
+							e.preventDefault();
+						}
+
+						return false;
+					}
+				}
+			};
+
+			this.allShortcuts[shortcutCombination] = {
+				callback: handler,
+				target: targetElement,
+				event: option.type
+			};
+
+			if (targetElement.addEventListener) {
+				targetElement.addEventListener(option.type, handler, false);
+			} else if (targetElement.attachEvent) {
+				targetElement.attachEvent('on' + option.type, handler);
+			} else {
+				targetElement['on' + option.type] = handler;
+			}
+		}
+	}, {
+		key: 'remove',
+		value: function remove(shortcutCombination) {
+			shortcutCombination = shortcutCombination.toLowerCase();
+			var binding = this.allShortcuts[shortcutCombination];
+			delete this.allShortcuts[shortcutCombination];
+			if (!binding) {
+				return;
+			}
+
+			var type = binding.event;
+			var targetElement = binding.target;
+			var callback = binding.callback;
+
+			if (targetElement.detachEvent) {
+				targetElement.detachEvent('on' + type, callback);
+			} else if (targetElement.removeEventListener) {
+				targetElement.removeEventListener(type, callback, false);
+			} else {
+				targetElement['on' + type] = false;
+			}
+		}
+	}]);
+
+	return Keycuts;
+}();
+
+var keycuts = new Keycuts();
+
+module.exports = keycuts;
--- src/keycuts.es6
+++ src/keycuts.es6
@@ -1,223 +1,263 @@
-/**
- * http://www.openjs.com/scripts/events/keyboard_shortcuts/
- * Version : 2.01.B
- * By Binny V A
- * License : BSD
- */
-shortcut = {
-	'all_shortcuts':{},//All the shortcuts are stored in this array
-	'add': function(shortcut_combination,callback,opt) {
-		//Provide a set of default options
-		var default_options = {
-			'type':'keydown',
-			'propagate':false,
-			'disable_in_input':false,
-			'target':document,
-			'keycode':false
-		}
-		if(!opt) opt = default_options;
-		else {
-			for(var dfo in default_options) {
-				if(typeof opt[dfo] == 'undefined') opt[dfo] = default_options[dfo];
-			}
-		}
+import _ from 'underscore';
 
-		var ele = opt.target;
-		if(typeof opt.target == 'string') ele = document.getElementById(opt.target);
-		var ths = this;
-		shortcut_combination = shortcut_combination.toLowerCase();
+const SHIFTNUMS = {
+		'`': '~',
+		'1': '!',
+		'2': '@',
+		'3': '#',
+		'4': '$',
+		'5': '%',
+		'6': '^',
+		'7': '&',
+		'8': '*',
+		'9': '(',
+		'0': ')',
+		'-': '_',
+		'=': '+',
+		';': ':',
+		"'": '"',
+		',': '<',
+		'.': '>',
+		'/': '?',
+		'\\': '|'
+	};
 
-		//The function to be called at keypress
-		var func = function(e) {
+const SPECIALKEYS = {
+		esc: 27,
+		escape: 27,
+		tab: 9,
+		space: 32,
+		return: 13,
+		enter: 13,
+		backspace: 8,
+		scrolllock: 145,
+		scroll_lock: 145,
+		scroll: 145,
+		capslock: 20,
+		caps_lock: 20,
+		caps: 20,
+		numlock: 144,
+		num_lock: 144,
+		num: 144,
+		pause: 19,
+		break: 19,
+		insert: 45,
+		home: 36,
+		delete: 46,
+		end: 35,
+		pageup: 33,
+		page_up: 33,
+		pu: 33,
+		pagedown: 34,
+		page_down: 34,
+		pd: 34,
+		left: 37,
+		up: 38,
+		right: 39,
+		down: 40,
+		f1: 112,
+		f2: 113,
+		f3: 114,
+		f4: 115,
+		f5: 116,
+		f6: 117,
+		f7: 118,
+		f8: 119,
+		f9: 120,
+		f10: 121,
+		f11: 122,
+		f12: 123
+	};
+
+const MODIFIERS = {
+		shift: {
+			wanted: false,
+			pressed: false
+		},
+		ctrl: {
+			wanted: false,
+			pressed: false
+		},
+		alt: {
+			wanted: false,
+			pressed: false
+		},
+		meta: {
+			wanted: false,
+			pressed: false
+		}
+	};
+
+class Keycuts {
+	constructor() {
+		this.defaultShortcutOption = {
+			type: 'keydown',
+			propagate: false,
+			disableInInput: false,
+			target: document,
+			keycode: false
+		};
+		this.allShortcuts = {};
+	}
+
+	add(shortcutCombination, callback, option = {}) {
+		option = _.defaults(option, this.defaultShortcutOption);
+ 		shortcutCombination = shortcutCombination.toLowerCase();
+		let targetElement = _.isString(option.target) ? document.getElementById(option.target) : option.target;
+		let handler = e => {
+			let char, code, kp = 0,
+				modifiers = {
+					shift: {
+						wanted: false,
+						pressed: false
+					},
+					ctrl: {
+						wanted: false,
+						pressed: false
+					},
+					alt: {
+						wanted: false,
+						pressed: false
+					},
+					meta: {
+						wanted: false,
+						pressed: false
+					}
+				};
+
 			e = e || window.event;
-			
-			if(opt['disable_in_input']) { //Don't enable shortcut keys in Input, Textarea fields
-				var element;
-				if(e.target) element=e.target;
-				else if(e.srcElement) element=e.srcElement;
-				if(element.nodeType==3) element=element.parentNode;
 
-				if(element.tagName == 'INPUT' || element.tagName == 'TEXTAREA') return;
-			}
-	
-			//Find Which key is pressed
-			if (e.keyCode) code = e.keyCode;
-			else if (e.which) code = e.which;
-			var character = String.fromCharCode(code).toLowerCase();
-			
-			if(code == 188) character=","; //If the user presses , when the type is onkeydown
-			if(code == 190) character="."; //If the user presses , when the type is onkeydown
+			if(option.disableInInput) {
+				let element = e.target ? e.target : (e.srcElement ? e.srcElement : null);
 
-			var keys = shortcut_combination.split("+");
-			//Key Pressed - counts the number of valid keypresses - if it is same as the number of keys, the shortcut function is invoked
-			var kp = 0;
-			
-			//Work around for stupid Shift key bug created by using lowercase - as a result the shift+num combination was broken
-			var shift_nums = {
-				"`":"~",
-				"1":"!",
-				"2":"@",
-				"3":"#",
-				"4":"$",
-				"5":"%",
-				"6":"^",
-				"7":"&",
-				"8":"*",
-				"9":"(",
-				"0":")",
-				"-":"_",
-				"=":"+",
-				";":":",
-				"'":"\"",
-				",":"<",
-				".":">",
-				"/":"?",
-				"\\":"|"
-			}
-			//Special Keys - and their codes
-			var special_keys = {
-				'esc':27,
-				'escape':27,
-				'tab':9,
-				'space':32,
-				'return':13,
-				'enter':13,
-				'backspace':8,
-	
-				'scrolllock':145,
-				'scroll_lock':145,
-				'scroll':145,
-				'capslock':20,
-				'caps_lock':20,
-				'caps':20,
-				'numlock':144,
-				'num_lock':144,
-				'num':144,
-				
-				'pause':19,
-				'break':19,
-				
-				'insert':45,
-				'home':36,
-				'delete':46,
-				'end':35,
-				
-				'pageup':33,
-				'page_up':33,
-				'pu':33,
-	
-				'pagedown':34,
-				'page_down':34,
-				'pd':34,
-	
-				'left':37,
-				'up':38,
-				'right':39,
-				'down':40,
-	
-				'f1':112,
-				'f2':113,
-				'f3':114,
-				'f4':115,
-				'f5':116,
-				'f6':117,
-				'f7':118,
-				'f8':119,
-				'f9':120,
-				'f10':121,
-				'f11':122,
-				'f12':123
-			}
-	
-			var modifiers = { 
-				shift: { wanted:false, pressed:false},
-				ctrl : { wanted:false, pressed:false},
-				alt  : { wanted:false, pressed:false},
-				meta : { wanted:false, pressed:false}	//Meta is Mac specific
-			};
-                        
-			if(e.ctrlKey)	modifiers.ctrl.pressed = true;
-			if(e.shiftKey)	modifiers.shift.pressed = true;
-			if(e.altKey)	modifiers.alt.pressed = true;
-			if(e.metaKey)   modifiers.meta.pressed = true;
-                        
-			for(var i=0; k=keys[i],i<keys.length; i++) {
-				//Modifiers
-				if(k == 'ctrl' || k == 'control') {
-					kp++;
-					modifiers.ctrl.wanted = true;
-
-				} else if(k == 'shift') {
-					kp++;
-					modifiers.shift.wanted = true;
-
-				} else if(k == 'alt') {
-					kp++;
-					modifiers.alt.wanted = true;
-				} else if(k == 'meta') {
-					kp++;
-					modifiers.meta.wanted = true;
-				} else if(k.length > 1) { //If it is a special key
-					if(special_keys[k] == code) kp++;
-					
-				} else if(opt['keycode']) {
-					if(opt['keycode'] == code) kp++;
-
-				} else { //The special keys did not match
-					if(character == k) kp++;
-					else {
-						if(shift_nums[character] && e.shiftKey) { //Stupid Shift key bug created by using lowercase
-							character = shift_nums[character]; 
-							if(character == k) kp++;
-						}
-					}
+				if(element !== null && element.nodeType === 3) {
+					element = element.parentNode;
 				}
-			}
-			
-			if(kp == keys.length && 
-						modifiers.ctrl.pressed == modifiers.ctrl.wanted &&
-						modifiers.shift.pressed == modifiers.shift.wanted &&
-						modifiers.alt.pressed == modifiers.alt.wanted &&
-						modifiers.meta.pressed == modifiers.meta.wanted) {
-				callback(e);
-	
-				if(!opt['propagate']) { //Stop the event
-					//e.cancelBubble is supported by IE - this will kill the bubbling process.
-					e.cancelBubble = true;
-					e.returnValue = false;
-	
-					//e.stopPropagation works in Firefox.
-					if (e.stopPropagation) {
-						e.stopPropagation();
-						e.preventDefault();
-					}
+
+				if(element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
 					return false;
 				}
 			}
-		}
-		this.all_shortcuts[shortcut_combination] = {
-			'callback':func, 
-			'target':ele, 
-			'event': opt['type']
+
+			if(e.keyCode) {
+				code = e.keyCode;
+			} else if(e.which) {
+				code = e.which;
+			}
+
+			char = (code === 188 ? ',' : (code === 190 ? '.' : String.fromCharCode(code).toLowerCase()));
+
+			if(e.ctrlKey) {
+				modifiers.ctrl.pressed = true;
+			}
+			if(e.shiftKey) {
+				modifiers.shift.pressed = true;
+			}
+			if(e.altKey) {
+				modifiers.alt.pressed = true;
+			}
+			if(e.metaKey) {
+				modifiers.meta.pressed = true;
+			}
+
+			shortcutCombination.split('+').forEach((key) => {
+				switch(key) {
+					case 'ctrl':
+					case 'control':
+						kp++;
+						modifiers.ctrl.wanted = true;
+					break;
+					case 'shift':
+						kp++;
+						modifiers.shift.wanted = true;
+					break;
+					case 'alt':
+						kp++;
+						modifiers.alt.wanted = true;
+					break;
+					case 'meta':
+						kp++;
+						modifiers.meta.wanted = true;
+					break;
+					default:
+						if(key.length > 1 && SPECIALKEYS[key] === code) {
+							kp++;
+						} else if(option.keycode) {
+							kp++;
+						} else {
+							if(char === key) {
+								kp++;
+							} else {
+								if(SHIFTNUMS[char] && e.shiftKey) {
+									char = SHIFTNUMS[char];
+									if(char === k) {
+										kp++;
+									}
+								}
+							}
+						}
+					break;
+				}
+			});
+
+			if(kp == keys.length &&
+				modifiers.ctrl.pressed === modifiers.ctrl.wanted &&
+				modifiers.shift.pressed === modifiers.shift.wanted &&
+				modifiers.alt.pressed === modifiers.alt.wanted &&
+				modifiers.meta.pressed === modifiers.meta.wanted) {
+				callback(e);
+
+				if(!option.propagate) {
+					e.cancelBubble = true;
+					e.returnValue = false;
+
+					if(e.stopPropagation) {
+						e.stopPropagation();
+						e.preventDefault();
+					}
+
+					return false;
+				}
+			}
 		};
-		//Attach the function with the event
-		if(ele.addEventListener) ele.addEventListener(opt['type'], func, false);
-		else if(ele.attachEvent) ele.attachEvent('on'+opt['type'], func);
-		else ele['on'+opt['type']] = func;
-	},
 
-	//Remove the shortcut - just specify the shortcut and I will remove the binding
-	'remove':function(shortcut_combination) {
-		shortcut_combination = shortcut_combination.toLowerCase();
-		var binding = this.all_shortcuts[shortcut_combination];
-		delete(this.all_shortcuts[shortcut_combination])
-		if(!binding) return;
-		var type = binding['event'];
-		var ele = binding['target'];
-		var callback = binding['callback'];
+		this.allShortcuts[shortcutCombination] = {
+			callback: handler,
+			target: targetElement,
+			event: option.type
+		};
 
-		if(ele.detachEvent) ele.detachEvent('on'+type, callback);
-		else if(ele.removeEventListener) ele.removeEventListener(type, callback, false);
-		else ele['on'+type] = false;
+		if(targetElement.addEventListener) {
+			targetElement.addEventListener(option.type, handler, false);
+		} else if(targetElement.attachEvent) {
+			targetElement.attachEvent(`on${option.type}`, handler);
+		} else {
+			targetElement[`on${option.type}`] = handler;
+		}
+	}
+
+	remove(shortcutCombination) {
+		shortcutCombination = shortcutCombination.toLowerCase();
+		let binding = this.allShortcuts[shortcutCombination];
+		delete(this.allShortcuts[shortcutCombination])
+		if(!binding) {
+			return;
+		}
+
+		let type = binding.event;
+		let targetElement = binding.target;
+		let callback = binding.callback;
+
+		if(targetElement.detachEvent) {
+			targetElement.detachEvent(`on${type}`, callback);
+		} else if(targetElement.removeEventListener) {
+			targetElement.removeEventListener(type, callback, false);
+		} else {
+			targetElement[`on${type}`] = false;
+		}
 	}
 }
+
+let keycuts = new Keycuts();
+
+module.exports = keycuts;
